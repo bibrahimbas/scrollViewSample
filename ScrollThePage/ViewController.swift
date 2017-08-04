0b46f3a77b67011a20e46185a6743520a22767eb
mac <belmaibrahimbas@Belma-MacBook-Pro.local>
2017-08-04 22:54:15 +0300

    Initial Commit

---
+++ ScrollThePage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScrollThePage
+//
+//  Created by Belma İbrahimbaş on 4.08.2017.
+//  Copyright © 2017 Belma Ibrahimbas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
