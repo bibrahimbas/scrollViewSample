9b1895933c0674c3e9bb0a0bffe7ae0c7ed33d89
mac <belmaibrahimbas@Belma-MacBook-Pro.local>
2017-08-05 22:56:39 +0300

    scrollView sample

--- ScrollThePage/ViewController.swift
+++ ScrollThePage/ViewController.swift
@@ -9,15 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    var images = [UIImageView]()
+    var contentWidth: CGFloat = 0.0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    
+            }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        for x in 0...2 {
+            let image = UIImage(named: "icon\(x).png")
+            let imageview = UIImageView(image: image)
+            images.append(imageview)
+            
+            var newX: CGFloat = 0.0
+            newX = scrollView.frame.size.width / 2 + scrollView.frame.size.width * CGFloat(x)
+            contentWidth += newX
+            
+            scrollView.addSubview(imageview)
+            imageview.frame = CGRect(x: newX - 75, y: (scrollView.frame.size.height / 2) - 75, width: 150, height: 150)
+        }
+        
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
+        scrollView.clipsToBounds = false
+        
+
     }
 
 
